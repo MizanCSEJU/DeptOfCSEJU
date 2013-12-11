330f552dca2caf7f3db2ead376fe5c804ab8727e
Mizanur Rahman <mizanur_1405@yahoo.com>
2013-12-11 10:52:21 +0600

    MyFirst class created.

---
+++ src/java/git/MyFirst.java
@@ -0,0 +1,13 @@
+package java.git;
+
+public class MyFirst {
+	String firstGit;
+	public MyFirst(){}
+	public String getFirstGit() {
+		return firstGit;
+	}
+	public void setFirstGit(String firstGit) {
+		this.firstGit = firstGit;
+	}
+	
+}
